1f496e389430025f8690b43d11f5879bde13d957
SEBASTIAN <sebastian.chicata@gmail.com>
2026-05-30 22:12:36 -0500

    Avance en el archivo UpdateTodo

--- src/domain/dtos/todos/update-todo.dto.ts
+++ src/domain/dtos/todos/update-todo.dto.ts
@@ -2,27 +2,45 @@
 export class UpdateTodoDto {
 
     private constructor(
+        public readonly id: number,
         public readonly text?: string,
-        public readonly completedAt?: string
+        public readonly completedAt?: Date
 
     ){}
 
-    static create( props: {[key:string]: any} ): [(string | undefined), (UpdateTodoDto | undefined)?]{
+    get values() {
+        const returnObj: {[key: string]: any} = {};
 
-        if ( !props ) return ['Body is required', undefined];
+        if( this.text ) returnObj.text = this.text;
+        if( this.completedAt ) returnObj.completedAt = this.completedAt;
 
-        const { text, completedAt } = props
-        let newCompletedAt = completedAt;
+        return returnObj
 
-        if( completedAt ){
-            const newCompletedAt = new Date(completedAt)
-            if( newCompletedAt.toString() === 'Invalid Date'){
-                return ['CompletedAt must be a valid date']
-            }
-        }
-
-        return [undefined, new UpdateTodoDto(text, newCompletedAt)]
     }
 
+    static create( props: {[key:string]: any} ): [(string | undefined), (UpdateTodoDto | undefined)?]{
+
+    if ( !props ) return ['Body is required', undefined];
+
+    const { id, text, completedAt } = props;
+    
+    let newCompletedAt: Date | undefined; 
+
+    if( !id || isNaN( Number(id) ) ){
+        return ['id must be a valid number'];
+    } 
+
+    if( completedAt ){
+        const parsedDate = new Date(completedAt); 
+        
+        if( parsedDate.toString() === 'Invalid Date'){
+            return ['CompletedAt must be a valid date'];
+        }
+        
+        newCompletedAt = parsedDate; 
+    }
+
+    return [undefined, new UpdateTodoDto(id, text, newCompletedAt)];
+}
 }
 
